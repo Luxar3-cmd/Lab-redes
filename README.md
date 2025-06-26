0e516647c85f114a9f94982e160e91c8585c6676
Luxar3-cmd <eduardo.moraless@usm.cl>
2025-06-26 19:20:10 -0400

    Implementación de Cifrado Asimétrico

--- README.md
+++ README.md
@@ -5,3 +5,22 @@ Para compilar:
 ```bash
 make all
 ```
+
+Luego ejecutar 
+
+```bash
+./lab1
+```
+
+## Transformación de claves .pem a .der
+
+
+### Clave privada
+```bash
+openssl rsa -in lyra_privada.pem -out lyra_privada.der -outform DER
+```
+
+### Clave pública
+```bash
+openssl pkey -pubin  -in nombre_clave_publica.pem -out nombre_clave_publica.der -outform DER 
+``` 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 
+#include <cryptopp/oaep.h>
 #include "rsa_utils.h"
 #include "aes_utils.h"
 #include "firmas.h"
@@ -55,14 +56,24 @@ void GenerateAESKeyAndIV(SecByteBlock & key, SecByteBlock& iv) {
 
 
 int main() {
+
+    /*-----------------------------------------------------------------------------------------------------------------------*/
+
     // Sección 1: Cifrado Simétrico con AES: Simulando un cifrado de un mensaje con AES-128 en modo CBC
     // Mensaje a cifrar
     cout << endl;
     cout << endl;
+    cout << "--------------------------------------------------------------------------------------" << endl;
     cout << " ----------------------------- Cifrado Simétrico con AES -----------------------------" << endl;
+    cout << "--------------------------------------------------------------------------------------" << endl;
+    cout << endl;
+    cout << endl;
+
     string mensaje = "La cámara descansa bajo el sauce llorón en el jardín del martillo.";
     cout << "Mensaje a cifrar: " << mensaje << endl;
+    cout << endl;
     cout << "--------------------------------------------------------------------------------------" << endl;
+    cout << endl;
 
     // Clave de 16 bytes para AES-128
     CryptoPP::byte clave[CryptoPP::AES::DEFAULT_KEYLENGTH] = {
@@ -81,19 +92,150 @@ int main() {
     string cifradoAES = aes_encrypt(mensaje, claveAES, iv); // Obtención del texto cifrado
     /*-----------------------------------------------------------------------------*/
     cout << "Texto cifrado (AES): " << cifradoAES << endl;
+    cout << endl;
     cout << "ivHex (IV en formato hexadecimal): " << BytesToHex(iv,iv.size()) << endl;
-    cout << " ------------------------- Ahora vamos a descifrar el texto cifrado con AES ------------------------- " << endl;
+
+    cout << "-----------------------------------------------------------------------------------------------------" << endl;
+    cout << "-------------------------- Ahora vamos a descifrar el texto cifrado con AES -------------------------" << endl;
+    cout << "-----------------------------------------------------------------------------------------------------" << endl;
+    cout << endl;
+    cout << endl;
     string mensajeRecuperadoAES = aes_decrypt(cifradoAES, claveAES, iv); // Descifrado del texto cifrado
     cout << "Texto recuperado (AES): " << mensajeRecuperadoAES << endl;
-    cout << "--------------------------------------------------------------------------------------" << endl;
-    cout << "--------------------------------------------------------------------------------------" << endl;
+    cout << endl;
+    cout << endl;
+    cout << "-----------------------------------------------------------------------------------------------------" << endl;
+    cout << "-                                            Fin demostración AES                                   -" << endl;
+    cout << "-----------------------------------------------------------------------------------------------------" << endl;
+    cout << endl;
     cout << endl;
 
+    /*-----------------------------------------------------------------------------------------------------------------------*/
+
+    // Sección 2: Cifrado Asimétrico con RSA: Simulando un cifrado de un mensaje con RSA
+
+
+    /*
+        NOTA: En vez de utilizar claves en formato PEM, se utilizarán archivos DER para las claves públicas y privadas. Al parecer 
+        son mas ligeras y permiten cifrar sin problemas.
+
+        Por poblemas sobre el largo de las claves, se simula que tango la firma como el mensaje se envían por separado.
+    */
+    cout << "--------------------------------------------------------------------------------------" << endl;
+    cout << " ----------------------------- Cifrado Asimétrico con RSA -----------------------------" << endl;
+    cout << "--------------------------------------------------------------------------------------" << endl;
+
+    
+    cout << endl;
+    mensaje = "Los archivos antiguos, código MPSH476, revelan la ubicación del séptimo pergamino perdido.";
+    cout << " Mensaje a cifrar: " << mensaje << endl;
+    cout << endl;
+    
+    string firma; 
+    string mensajeCifrado;
+
+    // Cargar clave privada lyra para firmar
+    RSA::PrivateKey clavePrivadaLyra; // Clave privada de Lyra
+    FileSource filePriv("Claves/lyra_privada.der", true); // Cargar clave privada de Lyra desde archivo DER
+    clavePrivadaLyra.BERDecode(filePriv);
+
+    // Firmar el mensaje
+
+    RSASS<PSS, SHA256>::Signer firmanteLyra(clavePrivadaLyra); // Crear el firmante RSA
+    StringSource(mensaje, true,
+        new SignerFilter(prng, firmanteLyra,
+            new StringSink(firma) // Firmar el mensaje
+        )
+    );
+
+    cout << "Firma generada (hex)" << binToHex(firma) << endl;
+    cout << endl;
+    // Cargar clave pública del Gran Maestro para cifrar
+    RSA::PublicKey clavePublicaGM; // Clave pública del Gran Maestro
+    FileSource filePub("Claves/gm_publica.der", true); 
+    clavePublicaGM.BERDecode(filePub); // Cargar clave pública del Gran Maestro desde archivo DER
+
+    // Cifrar el mensaje con la clave pública del Gran Maestro
+    RSAES_OAEP_SHA_Encryptor cifrador(clavePublicaGM);
+    StringSource(mensaje,true, 
+        new PK_EncryptorFilter(prng, cifrador, 
+            new StringSink(mensajeCifrado) // Cifrar el mensaje
+        )
+    );
+    cout << "Mensaje cifrado (RSA): " << binToHex(mensajeCifrado) << endl;
+    cout << endl;
+
+    cout << " --- Simulación Recepción ---" << endl;
+
+    // Cargar clave privada del Gran Maestro para descifrar
+    RSA::PrivateKey clavePrivadaGM; // Clave privada del Gran Maestro
+    FileSource filePrivGM("Claves/gm_privada.der", true); // Cargar clave privada del Gran Maestro desde archivo DER
+    clavePrivadaGM.BERDecode(filePrivGM);
+
+    // Descifrar el mensaje
+    string mensajeDescifrado; 
+    RSAES_OAEP_SHA_Decryptor descifrador(clavePrivadaGM); // Crear el descifrador RSA
+    StringSource(mensajeCifrado, true,
+        new PK_DecryptorFilter(prng, descifrador,
+            new StringSink(mensajeDescifrado) // Descifrar el mensaje
+        )
+    );
+    cout << endl;
+    cout << "Mensaje descifrado (RSA): " << mensajeDescifrado << endl;
+    cout << endl;
+    // Cargar calve pública de Lyra para verificar la firma
+    RSA::PublicKey clavePublicaLyra; // Clave pública de Lyra
+    FileSource filePubLyra("Claves/lyra_publica.der", true);
+    clavePublicaLyra.BERDecode(filePubLyra); // Cargar clave pública
+
+    // Verificar la firma
+    RSASS<PSS, SHA256>::Verifier verificador(clavePublicaLyra); // Crear el verificador RSA
+    bool firmaValida = verificador.VerifyMessage(
+        (const CryptoPP::byte*)mensajeDescifrado.data(), mensajeDescifrado.size(),
+        (const CryptoPP::byte*)firma.data(), firma.size()
+    );
+
+    if (firmaValida) {
+        cout << endl;
+        cout << "Firma verificada correctamente." << endl;
+        cout << endl;
+        cout << "Mensaje recibido: " << mensajeDescifrado << endl;
+        cout << endl;
+    } else {
+        cout << "Firma inválida." << endl;
+        cout << endl;
+    }
+    
+    
+    cout << "--------------------------------------------------------------------------------------" << endl;
+    cout << "-------------------------- Fin demostración RSA -------------------------------------" << endl;
+    cout << "--------------------------------------------------------------------------------------" << endl;
+    cout << endl;
+    cout << endl;
 
     /*-----------------------------------------------------------------------------------------------------------------------*/
 
     // Cifrado Asimétrico con RSA: Simulando un cifrado de un mensaje con RSA
-    cout << " ----------------------------- Cifrado Asimétrico con RSA -----------------------------" << endl;
+    cout << " --------------------------------------------------------------------------------------" << endl;
+    cout << "        ----------------------------- Canal Seguro -----------------------------      " << endl;
+    cout << " --------------------------------------------------------------------------------------" << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     string mensajeSecreto = "Los archivos antiguos, código MPSH476, revelan la ubicación del séptimo pergamino perdido"; // Mensaje proveniente de la hermana Lyra
     cout << "Mensaje a cifrar: " << mensajeSecreto << endl;
     cout << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -" << endl;
